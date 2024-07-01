c67819f05c053c0976c3495d692116ca5dbb6b63
rcannood <rcannood@users.noreply.github.com>
2024-07-01 11:04:31 +0000

    deploy: 75f8ec56dfefa03544380a8dbb414ff0579a3d90

---
+++ scripts/sync_resources.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# download data
+common/sync_resources/sync_resources \
+  --input s3://openproblems-data/resources_test/cell_cell_communication \
+  --output resources
+
+aws s3 sync resources \
+  s3://openproblems-data/resources_test/cell_cell_communication \
+  --delete \
+  --dryrun
