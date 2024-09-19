49c9cf483adbee5781b939b0ce7e7f9e84146292
Robrecht Cannoodt <rcannood@gmail.com>
2024-09-19 07:35:25 +0200

    rework infer_truth

--- src/data_processors/infer_truth/script.py
+++ src/data_processors/infer_truth/script.py
@@ -3,7 +3,7 @@ import liana as li
 
 ## VIASH START
 par = {
-  "input": "resources_test/task_cell_cell_communication/slidetags_human_brain/raw_spatial.h5ad",
+  "input": "resources_test/common/singlecell_broadinstitute_scp2167_human_brain/dataset.h5ad",
   "output": "output.h5ad"
 }
 meta = {
@@ -17,32 +17,31 @@ from funs import non_mirrored_product, onehot_groupby, format_truth
 
 # read the dataset
 adata = ad.read_h5ad(par["input"])
-adata.X = adata.layers["counts"]
+adata.X = adata.layers["normalized"]
 adata.var.index = adata.var["feature_name"]
-
-# Get needed params
-groupby = 'cell_type' 
-organism = adata.uns['dataset_organism']
 
 # one hot encode cell types
 li.ut.spatial_neighbors(adata, bandwidth=1000, max_neighbours=10)
-ctdata = onehot_groupby(adata, groupby=groupby)
 
+# get organism
 organism = adata.uns['dataset_organism']
 resource_name_map = {
   "homo_sapiens": "consensus",
   "mus_musculus": "mouseconsensus"
 }
+resource_name = resource_name_map[organism]
 
+# run LR
 lr = li.mt.bivariate(adata,
                      global_name='morans',
                      local_name=None,
                      use_raw=False,
-                     resource_name=resource_name_map[organism],
+                     resource_name=resource_name,
                      verbose=True,
                      n_perms=1000)
 
-# Infer Co-localized Cell types
+# run CP
+ctdata = onehot_groupby(adata, groupby='cell_type')
 interactions = non_mirrored_product(ctdata.var.index, ctdata.var.index)
 ct = li.mt.bivariate(ctdata,
                      global_name='morans',
@@ -61,6 +60,7 @@ ct = format_truth(ct, x_name='source', y_name='target')
 
 # cross join
 assumed_truth = lr.assign(key=1).merge(ct.assign(key=1), on='key')
+
 # simplify the dataframe
 assumed_truth['colocalized'] = assumed_truth['truth_y'] * assumed_truth['truth_x']
 assumed_truth = assumed_truth[assumed_truth['colocalized']]
