dec59ad71cf639c2e3c78489afd11f7e8b904a6d
Robrecht Cannoodt <rcannood@gmail.com>
2024-09-19 07:30:07 +0200

    add workflow to execute some basic preprocessing

--- scripts/create_resources/common_singlecell_broadinstitute.sh
+++ scripts/create_resources/common_singlecell_broadinstitute.sh
@@ -6,36 +6,34 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-# # remove this when you have implemented the script
-# echo "TODO: replace the commands in this script with the sequence of components that you need to run to generate test_resources."
-# echo "  Inside this script, you will need to place commands to generate example files for each of the 'src/api/file_*.yaml' files."
-# exit 1
-
 set -e
 
-RAW_DATA=resources/raw_data/singlecell_broadinstitute
-DATASET_ID=singlecell_broadinstitute_scp2167_human_brain
-DATASET_DIR=resources_test/common
+cat > /tmp/params.yaml << HERE
+param_list:
+  - id: singlecell_broadinstitute_scp2167_human_brain
+    raw_data_dir: resources/raw_data/singlecell_broadinstitute/SCP2167
+    dataset_name: "Human brain Slide-tags snRNA-seq"
+    dataset_url: "https://singlecell.broadinstitute.org/single_cell/study/SCP2167"
+    dataset_reference: "10.1038/s41586-023-06837-4"
+    dataset_summary: "Slide-tags snRNA-seq data on the human brain."
+    dataset_description: "..."
+    dataset_organism: "homo_sapiens"
 
-mkdir -p $DATASET_DIR
+do_subsample: true
+n_obs: 800
+n_vars: 1000
 
-# turn raw data into spatial anndata file
-viash run src/datasets/loaders/singlecell_broadinstitute/config.vsh.yaml -- \
-  --raw_data_dir "$RAW_DATA/SCP2167" \
-  --dataset_id $DATASET_ID \
-  --dataset_name "Human brain Slide-tags snRNA-seq" \
-  --dataset_url "https://singlecell.broadinstitute.org/single_cell/study/SCP2167" \
-  --dataset_reference "10.1038/s41586-023-06837-4" \
-  --dataset_summary "Slide-tags snRNA-seq data on the human brain." \
-  --dataset_description "..." \
-  --dataset_organism homo_sapiens \
-  --output "$DATASET_DIR/$DATASET_ID/dataset.h5ad"
-
-# write manual state.yaml. this is not actually necessary but you never know it might be useful
-cat > $DATASET_DIR/$DATASET_ID/state.yaml << HERE
-id: $DATASET_ID
-dataset: !file dataset.h5ad
+output_dataset: "\$id/dataset.h5ad"
+output_meta: "\$id/dataset_meta.yaml"
+output_state: "\$id/state.yaml"
+publish_dir: resources_test/common
 HERE
+
+nextflow run . \
+  -main-script target/nextflow/datasets/workflows/process_singlecell_broadinstitute/main.nf \
+  -profile docker \
+  -resume \
+  -params-file /tmp/params.yaml
 
 # only run this if you have access to the openproblems-data bucket
 aws s3 sync --profile op \
